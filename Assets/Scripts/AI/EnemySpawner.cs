07b7b2e4cf73b332ce88a8aa3a64b8dc2299f25b
NickFrangie <51765298+NickFrangie@users.noreply.github.com>
2021-05-01 09:49:09 -0400

    Enemy Spawning Tool

---
+++ Assets/Scripts/AI/EnemySpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // Components & References
+    private ParticleSystem spawnParticles;
+    private Animation spawnAnimation;
+
+
+    private void Start()
+    {
+        spawnAnimation = GetComponent<Animation>();
+        spawnParticles = GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) spawnAnimation.Play();
+    }
+
+    public void PlayParticleSystem() 
+    {
+        spawnParticles.Play();
+    }
+
+    public void DetachSpawn() 
+    {
+        Transform enemy = null;
+        foreach (Transform child in transform) {
+            if (child.gameObject.tag == "Enemy") {
+                enemy = child;
+            }
+        }
+        if (enemy != null) {
+            enemy.parent = transform.parent;
+            enemy.GetComponentInChildren<Animator>().enabled = true;
+        } 
+
+        Destroy(this.gameObject);
+    }
+}
