8d30af97cbc0c1ef1097b28ad9b7fb57fbc5897c
Jack McEvoy <jackmcevoy15@gmail.com>
2021-05-01 16:22:52 -0400

    Elevators

---
+++ Assets/Scripts/Gameplay/OutElevator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutElevator : MonoBehaviour
+{
+    public void SceneShift()
+    {
+        StartCoroutine(Object.FindObjectOfType<SceneControl>().SceneTransition());
+    }
+}
