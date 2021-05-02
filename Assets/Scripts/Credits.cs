b6736723f92379a9d428e4c50d2b3f3845d3ac88
BenjaminSlattery <50343293+BenjaminSlattery@users.noreply.github.com>
2021-05-02 13:46:15 -0400

    Added Credits
    Update your names to be what you want

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    public RectTransform rect;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rect.Translate(Vector3.up * Time.deltaTime * speed);
+    }
+}
