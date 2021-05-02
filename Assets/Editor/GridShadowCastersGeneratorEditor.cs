eaaca62b90b4019e664b6655f8b97dbc37451e02
NickFrangie <51765298+NickFrangie@users.noreply.github.com>
2021-05-02 01:54:18 -0400

    Split ShadowGenerator Functionality
    Split Editor Functionality for ShadowGenerator to keep it working.

---
+++ Assets/Editor/GridShadowCastersGeneratorEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(GridShadowCastersGenerator))]
+public class GridShadowCastersGeneratorEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Generate")) {
+            var generator = (GridShadowCastersGenerator)target;
+
+            Undo.RecordObject(generator.shadowCastersContainer, "GridShadowCastersGenerator.generate"); // this does not work :(
+
+            var casters = generator.Generate();
+
+            // as a hack to make the editor save the shadowcaster instances, we rename them now instead of when theyre generated.
+
+            Undo.RecordObjects(casters, "GridShadowCastersGenerator name prefab instances");
+
+            for (var i = 0; i < casters.Length; i++) {
+                casters[i].name += "_" + i.ToString();
+            }
+        }
+    }
+}
