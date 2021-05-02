c5b98bcdc616ae69de394c77f606d7058aac083e
NickFrangie <51765298+NickFrangie@users.noreply.github.com>
2021-05-02 16:56:16 -0400

    Mid Work Music Manager

---
+++ Assets/Scripts/Core/SoundControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.Utils;
+
+namespace Game.Core
+{
+    public class SoundControl : MonoBehaviour
+    {
+        // Constants
+        private float MAX_VOLUME = .5f;
+        private float FADE_IN_TIME = 5f;
+        
+        // Variables
+        private AudioSource audioSource;
+        private AudioSource crossAudio;
+        
+
+        private void Start()
+        {
+            audioSource = GetComponent<AudioSource>();
+            audioSource.volume = 0;
+            audioSource.loop = true;
+
+            StartCoroutine(UtilFunctions.LerpCoroutine(FadeTheme, 0, 1, FADE_IN_TIME));
+        }
+
+        private void FadeTheme(float percent) {
+            audioSource.volume = MAX_VOLUME * percent;
+        }
+
+        private void CrossFadeTheme(float percent) {
+            audioSource.volume = MAX_VOLUME * (1 - percent);
+            crossAudio.volume = MAX_VOLUME * percent;
+        }
+
+        public IEnumerator CrossFade(AudioClip audioClip, float fadeTime) {
+            crossAudio = this.gameObject.AddComponent<AudioSource>();
+            crossAudio.volume = 0;
+            crossAudio.clip = audioClip;
+            crossAudio.loop = true;
+            crossAudio.Play();
+            
+            yield return StartCoroutine(UtilFunctions.LerpCoroutine(CrossFadeTheme, 0, 1, fadeTime));
+
+            AudioSource temp = audioSource;
+            audioSource = crossAudio;
+            Destroy(temp);
+            print("ran throug");
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Events/Events/MusicChangeEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.Core;
+
+
+namespace Game.Events
+{
+    public class MusicChangeEvent : Event
+    {
+        private bool triggered = false;
+
+        public bool singleTriggerEvent = true;
+        public float fadeTime = 2.5f;
+        public SoundControl soundControl;
+        public AudioClip bgm;
+
+
+        public override void RunEvent() {
+            if (!triggered || !singleTriggerEvent) {
+                triggered = true;
+                StartCoroutine(soundControl.CrossFade(bgm, fadeTime));
+                Destroy(this);
+            }
+        }
+    }
+}
